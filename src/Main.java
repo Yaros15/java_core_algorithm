722743fd1695153546b441a7343d7083789fb102
Yaros15 <108079325+Yaros15@users.noreply.github.com>
2025-02-06 13:31:15 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import service.sort.SortingByInsertion;
 
 public class Main {
     public static void main(String[] args) {
-        CarController carController = new CarController(new SortingByInsertion());
+        CarController carController = new CarController(new InsertionSort(), new BinarySearch());
         carController.carSort();
         carController.showSortedCars();
         System.out.println("Search auto by date 1995");
